0156d17da3ee57155f811826416ec40a0d37f407
Ankita Gupta <ankitagupta@Ankitas-MacBook-Air.local>
2020-08-24 13:37:10 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepoListComponent } from './modules/repository/components/repo-list/repo-list.component';
+import { RepoDetailComponent } from './modules/repository/components/repo-detail/repo-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'repo-list',
+    component: RepoListComponent
+  },
+  {
+    path: 'repo-detail/:owner/:repo',
+    component: RepoDetailComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/repo-list'
+  },
+  {
+    path: '**',
+    component: RepoListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
